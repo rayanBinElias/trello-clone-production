ef55fcf9ecc039f14094709362f70658219abc13
Rayan Bin Elias <bineliasmrb@realve.io>
2023-12-27 14:30:07 +0800

    fix ai

--- lib/formatTodoForAI.ts
+++ lib/formatTodoForAI.ts
@@ -1,23 +1,23 @@
-const formatTodoForAI = (board: Board) => {
-  //create a copy from board column
-  const todos =  Array.from(board.columns.entries());
+// const formatTodoForAI = (board: Board) => {
+//   //create a copy from board column
+//   const todos =  Array.from(board.columns.entries());
 
-  //flat the array
-  const flatArray = todos.reduce((map, [key, value]) => {
-    map[key] = value.todos;
-    return map;
-  }, {} as { [key in TypedColumn]: Todo[] });
+//   //flat the array
+//   const flatArray = todos.reduce((map, [key, value]) => {
+//     map[key] = value.todos;
+//     return map;
+//   }, {} as { [key in TypedColumn]: Todo[] });
 
-  //reduce to key: value length
-  const flatArrayCounted = Object.entries(flatArray).reduce(
-    (map, [key, value]) => {
-      map[key as TypedColumn] = value.length;
-      return map;
-    },
-    {} as { [key in TypedColumn]: number }
-  );
+//   //reduce to key: value length
+//   const flatArrayCounted = Object.entries(flatArray).reduce(
+//     (map, [key, value]) => {
+//       map[key as TypedColumn] = value.length;
+//       return map;
+//     },
+//     {} as { [key in TypedColumn]: number }
+//   );
 
-  return flatArrayCounted;
-};
+//   return flatArrayCounted;
+// };
 
-export default formatTodoForAI;
+// export default formatTodoForAI;
--- open.ai.ts
+++ open.ai.ts
@@ -1,7 +1,7 @@
-import OpenAI from "openai";
+// import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPEN_AI_KEY,
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.OPEN_AI_KEY,
+// });
 
-export default openai;
+// export default openai;
